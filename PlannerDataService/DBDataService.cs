9e9873fe94119efafec3be5967067ef140582aa5
Heusraph <raphaelbautista2@gmail.com>
2025-06-13 23:30:28 +0800

    Create DBDataService.cs

---
+++ PlannerDataService/DBDataService.cs
@@ -0,0 +1,35 @@
+﻿using PlannerCommon;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+
+namespace PlannerDataService
+{
+    class DBDataService : IPlannerDataService
+    {
+        //connectionString
+        static string connectionString
+       = "Data Source =BAUTISTA\\SQLEXPRESS; Initial Catalog = ATM; Integrated Security = True; TrustServerCertificate=True;";
+
+        static SqlConnection sqlConnection;
+
+        public void AddProfile(PlannerProfile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlannerProfile> GetProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProfile(PlannerProfile profileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProfile(PlannerProfile profile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
